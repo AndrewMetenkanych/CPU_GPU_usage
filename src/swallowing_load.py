9cc23e01c331a4907c52dd82a553bbb1f75c2470
Andrew Metenkanych <andrew.metenkanych@gmail.com>
2024-08-12 19:21:14 +0300

    added infinite cycle for continuous sending

--- src/swallowing_load.py
+++ src/swallowing_load.py
@@ -1,6 +1,7 @@
 import psutil
 import GPUtil
 import requests
+import time
 
 def get_cpu_usage():
     return psutil.cpu_percent(interval=1)
@@ -18,19 +19,25 @@ def get_gpu_load_and_temp():
         return None, None  # Якщо GPU немає
 
 def main():
-    cpu_usage = get_cpu_usage()
-    ram_usage = get_ram_usage()
-    gpu_load, gpu_temp = get_gpu_load_and_temp()
+    while True:
+        cpu_usage = get_cpu_usage()
+        ram_usage = get_ram_usage()
+        gpu_load, gpu_temp = get_gpu_load_and_temp()
 
-    data = {
-        'cpu_usage': cpu_usage,
-        'ram_usage': ram_usage,
-        'gpu_load': gpu_load,
-        'gpu_temp': gpu_temp
-    }
-# Sending data on ESP32
-    response = requests.post("http://192.168.31.79/update", json=data)
-    print(response.status_code)
+        data = {
+            'cpu_usage': cpu_usage,
+            'ram_usage': ram_usage,
+            'gpu_load': gpu_load,
+            'gpu_temp': gpu_temp
+        }
+        
+        try:
+            response = requests.post("http://192.168.31.79/update", json=data)
+            print(f"Data sent! Status code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            print(f"Failed to send data: {e}")
+
+        time.sleep(0.5)  # Затримка в 5 секунд перед наступною відправкою даних
 
 if __name__ == "__main__":
     main()
